cf88ace9bbd1a74e590dce6f6dc67eedc0f904d4
ahmedmokhtar2030 <shoptown2022@gmail.com>
2023-10-18 14:44:07 +0300

    asdsad

--- conn.js
+++ conn.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 //Create Database Connection
 const conn = mysql.createConnection({
 	host: "mysql-140220-0.cloudclusters.net",
-	user: "admin",
-	password: "kXDuUOmN",
+	user: "client",
+	password: "12345678",
 	database: "brightvision", 
 	port: 19608 ,
 	multipleStatements: true
